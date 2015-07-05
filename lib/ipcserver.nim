959349890772dee042952b5c1615b0b83256973f
Shlomi Matichin <shlomomatichin@gmail.com>
2015-07-05 17:30:31 +0300

    Lib started

---
+++ lib/ipcserver.nim
@@ -0,0 +1,83 @@
+import asyncdispatch
+import asyncnet
+import macaddress
+import logging
+import json
+from hex import nil
+
+type
+    Callback = proc(json: var JsonNode): JsonNode
+    IPCServer* = ref object
+        callback: Callback
+        socket: AsyncSocket
+
+proc close*(self: var IPCServer) =
+    if self == nil:
+        return
+    if self.socket == nil:
+        return
+    self.socket.close()
+    self.socket = nil
+
+proc serveConnectionHeader(self: var IPCServer, connection: AsyncSocket) =
+    var server = self
+    var future = connection.recv(sizeof(uint32))
+    future.callback = proc() =
+        if future.error != nil:
+            error("Error receiving header: " & future.error.msg)
+            connection.close()
+            return
+        if future.read.len == 0:
+            connection.close()
+            return
+        if future.read.len != sizeof(uint32):
+            error("Partial header received")
+            connection.close()
+            return
+        var header = future.read
+        var length = cast[ptr uint32](header[0].addr)[].int
+        var future2 = connection.recv(length)
+        future2.callback = proc() =
+            if future2.error != nil:
+                error("Error receiving data: " & future2.error.msg)
+                connection.close()
+                return
+            if future2.read.len != length:
+                error("Partial data received. Expected $1 was $2", length, future2.read.len)
+                connection.close()
+                return
+            var json = parseJson(future2.read)
+            var response: JsonNode
+            try:
+                response = server.callback(json)
+            except:
+                error("Error handling command: $1 error: $2", future2.read, getCurrentExceptionMsg())
+                response = %*{"status": "error", "message": getCurrentExceptionMsg()}
+            var serialized = $response
+            var responseHeader = newString(sizeof(uint32))
+            cast[ptr uint32](responseHeader[0].addr)[] = serialized.len.uint32
+            var future3 = connection.send(responseHeader & serialized)
+            future3.callback = proc() =
+                if future3.error != nil:
+                    error("Error sending data: " & future3.error.msg)
+                    connection.close()
+                    return
+            serveConnectionHeader(server, connection)
+
+proc serve(self: var IPCServer) =
+    var server = self
+    var future = self.socket.accept()
+    future.callback = proc() =
+        if future.error != nil:
+            error("Error serving IPC: " & future.error.msg)
+            return
+        serveConnectionHeader(server, future.read)
+        serve(server)
+
+proc newIPCServer*(port: int, callback: Callback): IPCServer =
+    result = IPCServer(callback: callback)
+    result.socket = newAsyncSocket()
+    result.socket.setSockOpt(OptReuseAddr, true)
+    result.socket.bindAddr(Port(port))
+    result.socket.listen(10)
+    serve(result)
---
+++ lib/safejson.nim
@@ -0,0 +1,25 @@
+import json
+import macros
+
+export json.JsonNode
+export json.`%*`
+export json.`%`
+export json.newJObject
+export json.newJString
+export json.`[]=`
+export json.`$`
+
+proc `[]`*(node: JsonNode; name: string): JsonNode {.raises: [], tags: [].} =
+    if node == nil:
+        return nil
+    return json.`[]`(node, name)
+
+proc getNum*(node: JsonNode; default: BiggestInt = 0): BiggestInt {.raises: [], tags: [].} =
+    if node == nil:
+        return default
+    return json.getNum(node, default)
+
+proc getStr*(node: JsonNode; default: string = ""): string {.raises: [], tags: [].} =
+    if node == nil:
+        return default
+    return json.getStr(node, default)
